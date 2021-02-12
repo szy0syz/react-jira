bdecab7ec4281c0baec1603d5a423fe494b56f90
szy0syz <szy0syz@gmail.com>
2021-02-12 10:20:56 +0800

    chore: any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 export const cleanObject = (obj: object) => {
   // 最好别去改变传入的对象

c251c0d517a7dee6ba9213087369e38887b68e50
szy0syz <szy0syz@gmail.com>
2021-02-13 15:32:32 +0800

    feat: 首页布局优化，grid和flex布局的应用场景

--- src/authenticated-app/index.tsx
+++ src/authenticated-app/index.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { useAuth } from 'context/auth-context';
 import React from 'react';
 import ProjectListScreen from 'screens/projext-list';
@@ -5,9 +6,42 @@ import ProjectListScreen from 'screens/projext-list';
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>Logo</HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  /* display: grid; */
+  /* grid-template-rows: 6rem 1fr; */
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  height: 6rem;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+  height: calc(100vh - 6rem);
+`;

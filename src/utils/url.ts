bad4fe04429b4e12c61cb7cf5875f00f9f4ac50a
szy0syz <szy0syz@gmail.com>
2021-06-13 21:27:14 +0800

    chore: 改变setSearchParams的实现方式

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
-import { cleanObject } from 'utils';
-import { URLSearchParamsInit, useSearchParams } from 'react-router-dom';
+import { useMemo } from "react";
+import { cleanObject } from "utils";
+import { URLSearchParamsInit, useSearchParams } from "react-router-dom";
 
 /**
  * 返回页面url中，指定键的参数值
@@ -8,22 +8,18 @@ import { URLSearchParamsInit, useSearchParams } from 'react-router-dom';
  * @returns
  */
 export const useUrlQueryParams = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
+  const [searchParams] = useSearchParams();
+  const setSearchParams = useSetUrlSearchParam();
   return [
     useMemo(
       () =>
         keys.reduce((prev, key) => {
-          return { ...prev, [key]: searchParams.get(key) || '' };
+          return { ...prev, [key]: searchParams.get(key) || "" };
         }, {} as { [key in K]: string }),
       [searchParams]
     ),
     (params: Partial<{ [key in K]: unknown }>) => {
-      const o = cleanObject({
-        ...Object.fromEntries(searchParams),
-        ...params,
-      }) as URLSearchParamsInit;
-      setSearchParams(o);
+      return setSearchParams(params);
     },
   ] as const; // 转成元组
 };

ca7f3041e82805393f22252102f9c4789d6f5ca2
szy0syz <szy0syz@gmail.com>
2021-06-19 23:00:10 +0800

    feat: 删除任务组功能

--- src/screens/epic/index.tsx
+++ src/screens/epic/index.tsx
@@ -1,5 +1,70 @@
-import React from 'react'
+import { Button, List, Modal } from "antd";
+import { Row, ScreenContainer } from "components/lib";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
+import { useProjectInUrl } from "screens/kanban/util";
+import { Epic } from "types/epic";
+import { useDeleteEpic, useEpics } from "utils/epic";
+import { useTasks } from "utils/task";
+import { useEpicQueryKey, useEpicSearchParams } from "./util";
 
 export const EpicScreen = () => {
-  return <h1>EPIC</h1>
-}
+  const { data: currentProject } = useProjectInUrl();
+  const { data: epics } = useEpics(useEpicSearchParams());
+  const { data: tasks } = useTasks({ projectId: currentProject?.id });
+  const { mutate: deleteEpic } = useDeleteEpic(useEpicQueryKey());
+
+  const confirmDeleteEpic = (epic: Epic) => {
+    Modal.confirm({
+      title: `确定删除项目组：${epic.name}`,
+      content: "点击确定删除",
+      okText: "确定",
+      onOk() {
+        deleteEpic({ id: epic.id });
+      },
+      cancelText: "取消"
+    });
+  };
+
+  return (
+    <ScreenContainer>
+      <h1>{currentProject?.name}</h1>
+      <List
+        dataSource={epics}
+        itemLayout="vertical"
+        renderItem={(epic) => (
+          <List.Item>
+            <List.Item.Meta
+              title={
+                <Row between>
+                  <span>{epic.name}</span>{" "}
+                  <Button type="link" onClick={() => confirmDeleteEpic(epic)}>
+                    删除
+                  </Button>
+                </Row>
+              }
+              description={
+                <div>
+                  <div>开始时间：{dayjs(epic.start).format("YYYY-MM-DD")}</div>
+                  <div>结束时间：{dayjs(epic.end).format("YYYY-MM-DD")}</div>
+                </div>
+              }
+            />
+            <div>
+              {tasks
+                ?.filter((task) => task.epicId === epic.id)
+                .map((task) => (
+                  <Link
+                    key={task.id}
+                    to={`/projects/${currentProject?.id}/kanban?editingTaskId=${task.id}`}
+                  >
+                    {task.name}
+                  </Link>
+                ))}
+            </div>
+          </List.Item>
+        )}
+      ></List>
+    </ScreenContainer>
+  );
+};
